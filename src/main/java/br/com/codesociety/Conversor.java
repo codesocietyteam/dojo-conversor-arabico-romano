cda1c051228c4660494a46cc698caa39710c9228
Paulo <leopoldes@gmail.com>
2016-09-02 19:59:50 -0300

    Primeiro passo para resolver teste do 25

--- src/main/java/br/com/codesociety/Conversor.java
+++ src/main/java/br/com/codesociety/Conversor.java
@@ -1,17 +1,43 @@
 package br.com.codesociety;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Conversor {
 
-	public String converte(int numero) {
-		if (numero == 1) {
-			return "I";			
-		} else if (numero == 2) {
-			return "II";
-		} else if (numero == 3) {
-			return "III";
+	private Map<Integer, String> romanosMap = new HashMap<Integer, String>();
+	private Map<Integer, String> romanosDezenasMap = new HashMap<Integer, String>(); 
+	
+	public String converte(int numero) { //25
+		construirAlfabeto();
+		
+		String num = String.valueOf(numero); //'25'
+		char[] digitos = num.toCharArray(); // ['2','5']
+		
+		String dezena = null;
+		if(digitos.length > 1){
+			dezena = romanosDezenasMap.get(digitos[0]);
 		}
 		
-		return null;
+		
+		
+		return romanosMap.get(numero);
+	}
+
+	private void construirAlfabeto() {
+		romanosMap.put(1, "I");
+		romanosMap.put(2, "II");
+		romanosMap.put(3, "III");
+		romanosMap.put(4, "IV");
+		romanosMap.put(5, "V");
+		romanosMap.put(6, "VI");
+		romanosMap.put(7, "VII");
+		romanosMap.put(8, "VIII");
+		romanosMap.put(9, "IX");
+		romanosMap.put(10, "X");
+		
+		romanosDezenasMap.put(10,"X");
+		romanosDezenasMap.put(20,"XX");
 	}
 
 }
